4ea128c7801d0b6864c5c897cbbb8798d518aa16
Gajalakshmi Ravi <121280076+GajalakshmiRavi@users.noreply.github.com>
2024-12-03 12:55:50 +0530

    Time: 606 ms (52.3%), Space: 0B (100%) - LeetHub

---
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select name as Customers
+from customers c
+where c.id not in
+(select c.id
+from customers c
+inner join orders o
+on c.id = o.customerid);
